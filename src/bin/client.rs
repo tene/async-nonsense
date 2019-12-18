40b95fbff3de032fc7ae7432aace2aed1b44a51a
Stephen Weeks <tene@allalone.org>
2019-12-18 14:20:10 -0800

    Basic Client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,18 @@
-use futures::{
-    pin_mut, select,
-    stream::{Stream, StreamExt},
-};
+use futures::{pin_mut, select, stream::StreamExt};
 use linefeed::{Interface, ReadResult};
 use std::sync::Arc;
-use tokio::{io, sync::mpsc::channel};
+use tokio::{
+    io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+    sync::mpsc::channel,
+};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let interface = Arc::new(Interface::new("chat")?);
     interface.set_prompt("» ")?;
     let interface_r = interface.clone();
-    let (mut lines_send, lines) = channel(128);
+    let (mut lines_send, input_lines) = channel(128);
     std::thread::spawn(move || loop {
         match interface_r.read_line() {
             Ok(ReadResult::Input(line)) => {
@@ -23,14 +24,25 @@ async fn main() -> io::Result<()> {
             _ => {}
         }
     });
-    let lines = lines.fuse();
-    pin_mut!(lines);
+    let input_lines = input_lines.fuse();
+    let mut socket = TcpStream::connect("127.0.0.1:8080").await?;
+    let (recv, send) = socket.split();
+    let mut server_lines = BufReader::new(recv).lines().fuse();
+    pin_mut!(input_lines, send);
     loop {
         select! {
-            line = lines.next() => match line {
-                Some(line) => writeln!(interface, "{}", line)?,
+            line = input_lines.next() => match line {
+                Some(line) => {
+                    let rv = send.write_all(format!("{}\n", line).as_bytes()).await?;
+                },
                 None => break,
-            }
+            },
+            line = server_lines.next() => match line {
+                Some(Ok(line)) => {
+                    writeln!(interface, "{}", line)?;
+                },
+                _ => break,
+            },
         }
     }
     Ok(())
